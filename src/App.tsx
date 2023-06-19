a1ffa61d75d54641a2f0f07a8575a3bccbb3dcc8
gabriel9silv <134931559+gabriel9silv@users.noreply.github.com>
2023-06-19 14:48:57 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import styles from './App.module.css';
 import poweredImage from './assets/powered.png'
-
 import {levels, calculateImc} from './helpers/imc'
+import { GridItem} from './components/GridItem'
 
 
 const App = () => {
@@ -49,9 +49,10 @@ const App = () => {
 
       <div className={styles.rightSide}>
         <div className={styles.grid}>
-          {levels.map( (item, key) => (
-            <div key={key}>{item.title}</div>
-          ))}
+        {levels.map( (item, key) => (
+        <GridItem key={key} item={item} />
+        ) )}
+
 
         </div>
       </div>
